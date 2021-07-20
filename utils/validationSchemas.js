fbd97b42760db4b88c6eed502d44e1a0a7de3bbf
Tajiboev <mukhammadjon.tajiboev@gmail.com>
2021-07-20 15:59:00 +0500

    schema to check req.body for registration

---
+++ utils/validationSchemas.js
@@ -0,0 +1,19 @@
+const Joi = require("joi");
+
+const registerClientSchema = Joi.object({
+	username: Joi.string().required(),
+	email: Joi.string().required(),
+	password: Joi.string().required(),
+	confirm: Joi.ref("password"),
+}).with("password", "confirm");
+
+const registerMarketerSchema = Joi.object({
+	name: Joi.string().required(),
+	email: Joi.string().required(),
+	website: Joi.string().required(),
+	category: Joi.string().required(),
+	password: Joi.string().required(),
+	confirm: Joi.ref("password"),
+}).with("password", "confirm");
+
+export { registerClientSchema, registerMarketerSchema };
