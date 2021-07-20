028b5ea9bb6396d1a4ffcac09e44acad81ac71cf
Tajiboev <mukhammadjon.tajiboev@gmail.com>
2021-07-20 14:09:22 +0500

    email is indexed and lowercase, added timestaps

--- models/Client.js
+++ models/Client.js
@@ -1,32 +1,37 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
 //create a schema
-const ClientSchema = new Schema({
-    username: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-        match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    products: [
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'product'
-        }
-    ],
-    date: {
-        type: Date,
-        default: Date.now
-    }
-})
+const ClientSchema = new Schema(
+	{
+		username: {
+			type: String,
+			required: true,
+		},
+		email: {
+			type: String,
+			required: true,
+			unique: true,
+			index: true,
+			lowercase: true,
+			match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/,
+		},
+		password: {
+			type: String,
+			required: true,
+		},
+		products: [
+			{
+				type: mongoose.Schema.Types.ObjectId,
+				ref: "product",
+			},
+		],
+		date: {
+			type: Date,
+			default: Date.now,
+		},
+	},
+	{ strictQuery: true, timestamps: true }
+);
 
-module.exports = Client = mongoose.model('client', ClientSchema)
+module.exports = Client = mongoose.model("client", ClientSchema);
