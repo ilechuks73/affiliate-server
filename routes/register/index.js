c0a711879b13eeba360725bc73900acd863607e8
Tajiboev <mukhammadjon.tajiboev@gmail.com>
2021-07-20 15:58:00 +0500

    using validateBody middleware

--- routes/register/index.js
+++ routes/register/index.js
@@ -1,11 +1,16 @@
-const express = require('express')
-const router = express.Router()
-const registerController = require('../../controllers/register')
-
+const express = require("express");
+const router = express.Router();
+const registerController = require("../../controllers/register");
+const { default: validateBody } = require("../../middleware/validateBody");
+const { registerClientSchema, registerMarketerSchema } = require("../../utils/validationSchemas");
 
 //process client's info
-router.post('/client', registerController.process_client_info)
+router.post("/client", validateBody(registerClientSchema), registerController.process_client_info);
 //process marketer's info
-router.post('/marketer', registerController.process_marketer_info)
+router.post(
+	"/marketer",
+	validateBody(registerMarketerSchema),
+	registerController.process_marketer_info
+);
 
-module.exports = router
+module.exports = router;
