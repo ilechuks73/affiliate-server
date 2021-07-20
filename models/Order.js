f44d71d6b74538d34a5d98aad346cc9ca33d015c
Tajiboev <mukhammadjon.tajiboev@gmail.com>
2021-07-20 14:09:53 +0500

    removed custom date, added timestamps

--- models/Order.js
+++ models/Order.js
@@ -1,25 +1,24 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
-const OrderSchema = new Schema({
-    product: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'product',
-        required: true
-    },
-    quantity: {
-        type: Number,
-        default: 1
-    },
-    created_by: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'marketer',
-        required: true
-    },
-    date: {
-        type: Date,
-        default: Date.now
-    }
-})
+const OrderSchema = new Schema(
+	{
+		product: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "product",
+			required: true,
+		},
+		quantity: {
+			type: Number,
+			default: 1,
+		},
+		created_by: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "marketer",
+			required: true,
+		},
+	},
+	{ strictQuery: true, timestamps: true }
+);
 
-module.exports = Order = mongoose.model('order', OrderSchema)
+module.exports = Order = mongoose.model("order", OrderSchema);
